ad83ee968537c622a5e5dea099faedd421e7faa0
Kévin Bénard <kv.benard@gmail.com>
2023-11-01 10:53:53 +0000

    Use the new source

--- models/marts/supply_chain/stock_model.sql
+++ models/marts/supply_chain/stock_model.sql
@@ -2,5 +2,5 @@ SELECT
     model_name,
     sum(stock) as stock,
     count(distinct product_id) as count_products
-FROM {{ref('stg_circle__stock')}}
+FROM {{ref('stg_google_sheets__raw_stock')}}
 GROUP BY 1
---
+++ models/staging/google_sheets/stg_google_sheets__raw_stock.sql
@@ -0,0 +1,27 @@
+with 
+
+source as (
+
+    select * from {{ source('google_sheets', 'raw_stock') }}
+
+),
+
+renamed as (
+
+    select
+        _row,
+        _fivetran_synced,
+        color,
+        color_name,
+        forecast_stock,
+        model,
+        model_name,
+        price,
+        size,
+        stock
+
+    from source
+
+)
+
+select * from renamed
